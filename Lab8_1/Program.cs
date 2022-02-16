625e031bdf754c491ce7d35c3e58be0b167a9bde
Proshayrus <gerlynd.prokhor@gmail.com>
2022-02-16 14:12:23 +0200

    first Task

---
+++ Lab8_1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+    public class Program
+    {
+        delegate int Average(int a, int b, int c);
+
+        static void Main(string[] args)
+        {
+            Average mid = (a, b, c) => (a + b + c) / 3;
+
+            Console.WriteLine(mid(423, 555, 109));
+            Console.ReadLine();
+        }
+    }
