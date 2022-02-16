460157fcee97eaace275af897b8d156221685232
Proshayrus <gerlynd.prokhor@gmail.com>
2022-02-16 14:28:16 +0200

    Second Task

---
+++ Lab8_2/Program.cs
@@ -0,0 +1,27 @@
+﻿delegate int ReturnsInt();
+delegate int Operation(ReturnsInt[] delegates);
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Operation op = (delegates) =>
+        {
+            long sum = 0;
+
+            foreach (var d in delegates)
+                sum += d();
+
+            return (int)sum / delegates.Length;
+        };
+
+        ReturnsInt[] delegates = new ReturnsInt[45];
+        Random rand = new Random();
+
+        for (int i = 0; i < delegates.Length; i++)
+            delegates[i] = rand.Next;
+
+        Console.WriteLine(op(delegates));
+        Console.ReadLine();
+    }
+}
